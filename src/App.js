378bfeb3077caf5c62b10c4b1f992d9272621ee4
seanmodd <sean@vestaridefair.com>
2021-10-11 16:11:13 -0700

    2 files modified, 3 added, 3 deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./App.scss";
-import Stepper from "./stepper/Stepper";
-import React, { Component } from "react";
+import './App.css';
+import Stepper from './stepper/Stepper';
+import React, { Component } from 'react';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentStep: 1
+      currentStep: 1,
     };
   }
 
   handleClick(clickType) {
     const { currentStep } = this.state;
     let newStep = currentStep;
-    clickType === "next" ? newStep++ : newStep--;
+    clickType === 'next' ? newStep++ : newStep--;
 
     if (newStep > 0 && newStep <= 5) {
       this.setState({
-        currentStep: newStep
+        currentStep: newStep,
       });
     }
   }
@@ -47,7 +47,7 @@ export default class App extends Component {
 
         <div className="buttons-container">
           <button onClick={() => this.handleClick()}>Previous</button>
-          <button onClick={() => this.handleClick("next")}>Next</button>
+          <button onClick={() => this.handleClick('next')}>Next</button>
         </div>
       </>
     );
@@ -56,8 +56,8 @@ export default class App extends Component {
 
 const stepsArray = [
   // "Create your account",
-  "Add personal info",
-  "Add payment details",
-  "Complete registration",
-  "Registration complete"
+  'Add personal info',
+  'Add payment details',
+  'Complete registration',
+  'Registration complete',
 ];
